bc0fc77c73184f9f0d1ee46c1352e5e866b6041e
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-22 23:18:34 +0200

    import css file into main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html", // This is good
+    "./src/**/*.{js,ts,jsx,tsx}", // Checks all files in the `src` folder
+    "./components/**/*.{js,ts,jsx,tsx}", // Add other folders if you have more
+  ],
   theme: {
     extend: {},
   },
