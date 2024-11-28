aa3ffc6f8c4b8bab757df5b0ef03d1850ecfc448
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-28 12:32:59 +0200

    update Footer.tsx and Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,52 +1,67 @@
+import { useState } from "react";
 import logo from "@assets/images/logo.svg";
 import { Link } from "react-router-dom";
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="sticky top-0 z-40">
-      <nav className=" border-gray-200 px-4 lg:px-6 py-2.5 bg-[#000] h-[130px]">
+    <header className="sticky top-0 z-50">
+      <nav className="border-gray-200 lg:px-6 py-2.5 bg-[#000] h-[130px] px-6 md:px-24">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          {/* Logo */}
           <Link to="/" className="flex items-center">
-            <img src={logo} className="mr-3  h-24" alt="Flowbite Logo" />
+            <img src={logo} className="mr-3 h-24" alt="Logo" />
           </Link>
+
+          {/* Hamburger Button */}
           <div className="flex items-center lg:order-2">
             <button
-              data-collapse-toggle="mobile-menu-2"
               type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
+              className="inline-flex items-center p-2 ml-1 text-2xl  text-sunset-orange rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              onClick={toggleMenu}
             >
               <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <svg
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              {isMenuOpen ? (
+                <svg
+                  className="w-10 h-10"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="w-10 h-10"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              )}
             </button>
           </div>
 
-          {/* main navbar */}
+
+          {/* Main Navbar */}
           <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } justify-between items-center w-full lg:flex lg:w-auto lg:order-1 bg-[#000000ad] pl-[30px] mt-[30px] duration-75`}
             id="mobile-menu-2"
           >
             <ul className="flex flex-col mt-4 font-medium text-[20px] lg:flex-row lg:space-x-8 lg:mt-0">
@@ -62,7 +77,7 @@ const Header = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-white  hover:text-[#fba43d] "
+                  className="block py-2 pr-4 pl-3 text-white hover:text-[#fba43d]"
                 >
                   About
                 </a>
@@ -70,7 +85,7 @@ const Header = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-white  hover:text-[#fba43d]  "
+                  className="block py-2 pr-4 pl-3 text-white hover:text-[#fba43d]"
                 >
                   Products
                 </a>
@@ -78,7 +93,7 @@ const Header = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-white  hover:text-[#fba43d]  "
+                  className="block py-2 pr-4 pl-3 text-white hover:text-[#fba43d]"
                 >
                   Blog
                 </a>
@@ -86,7 +101,7 @@ const Header = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-white  hover:text-[#fba43d]  "
+                  className="block py-2 pr-4 pl-3 text-white hover:text-[#fba43d]"
                 >
                   News
                 </a>
@@ -94,7 +109,7 @@ const Header = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-white  hover:text-[#fba43d] "
+                  className="block py-2 pr-4 pl-3 text-white hover:text-[#fba43d]"
                 >
                   Contact
                 </a>
@@ -106,4 +121,10 @@ const Header = () => {
     </header>
   );
 };
+
 export default Header;
+
+// background: #000000ad;
+// margin-top: 30px;
+// padding-left: 30px;
+// width: 100%;
