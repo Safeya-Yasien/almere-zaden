c855b9bd5ebc9b67b0dcbc20599f0fcaf0a87e68
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-28 10:08:40 +0200

    update Products.tsx

--- src/components/almereZaden/products/Products.tsx
+++ src/components/almereZaden/products/Products.tsx
@@ -63,25 +63,36 @@ const Products = () => {
 
       {/* Product Grid */}
       <div className="">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  mt-12 gap-x-8 gap-y-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  mt-12 gap-x-8 gap-y-12">
           {products.map((product) => (
-            <Link to="" key={product.id} className="flex flex-col gap-4">
-              <div className="relative w-full h-60">
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="object-cover hover:opacity-0 absolute z-10 transition-opacity duration-500 rounded-md w-full h-full"
-                />
-                <img
-                  src={product.hoverImage}
-                  alt={product.name}
-                  className="absolute object-cover rounded-md w-full h-full"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-between">
+            <div
+              key={product.id}
+              className="border border-[#eeeeee] rounded-md p-3 flex flex-col gap-6 
+               transition-all duration-500 hover:shadow-lg "
+            >
+              <Link
+                to=""
+                key={product.id}
+                className="flex flex-col gap-4 border rounded-md "
+              >
+                <div className="relative w-full h-60">
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    className="object-cover hover:opacity-0 absolute z-10 transition-opacity duration-500 rounded-md w-full h-full"
+                  />
+                  <img
+                    src={product.hoverImage}
+                    alt={product.name}
+                    className="absolute object-cover rounded-md w-full h-full"
+                  />
+                </div>
+              </Link>
+              {/* bg-[#F7F7F7] */}
+              <div className="flex flex-col items-center justify-between pb-4 ">
                 <span className="font-medium">{product.name}</span>
               </div>
-            </Link>
+            </div>
           ))}
         </div>
       </div>
