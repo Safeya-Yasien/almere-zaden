b3efb016da688c7ab583334f37c01095ca55386b
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-28 06:00:29 +0200

    feat: create src/components/almereZaden/index.ts

---
+++ src/components/almereZaden/index.ts
@@ -0,0 +1,9 @@
+import Hero from "./Hero";
+import Vision from "./Vision";
+import Categories from "./categories/Categories";
+import Services from "./Services/Services";
+
+export {Hero, Vision,
+    Categories,Services
+
+}
