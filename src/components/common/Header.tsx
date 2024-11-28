bd75f7ead6660fe1b8451e9753de3af849c21c77
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-28 06:03:03 +0200

    update Header.tsx remove unused links

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,14 +8,6 @@ const Header = () => {
             <img src={logo} className="mr-3  h-24" alt="Flowbite Logo" />
           </a>
           <div className="flex items-center lg:order-2">
-            <a
-              href="#"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            ></a>
-            <a
-              href="#"
-              className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-            ></a>
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
@@ -50,6 +42,8 @@ const Header = () => {
               </svg>
             </button>
           </div>
+
+          {/* main navbar */}
           <div
             className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
             id="mobile-menu-2"
