d67b036c284449617449f1f5af731a4861b2c14e
Safeya-Yasien <ssafeya5@gmail.com>
2024-11-28 05:59:05 +0200

    create 8 files create hero section

---
+++ src/components/almereZaden/Hero.tsx
@@ -0,0 +1,60 @@
+import swiper6 from "@assets/images/swiper2.jpg";
+import swiper5 from "@assets/images/swiper3.jpg";
+import swiper4 from "@assets/images/swiper5.jpg";
+import swiper3 from "@assets/images/swiper1.jpg";
+import swiper1 from "@assets/images/swiper6.jpg";
+import swiper2 from "@assets/images/swiper7.jpg";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
+// Slider component
+const Hero = () => {
+  return (
+    <Swiper
+      spaceBetween={30}
+      modules={[Navigation]}
+      className="w-[100%] h-[84vh] m-0"
+    >
+      <SwiperSlide>
+        <SwiperSlice image={swiper1} text={"Title 1"} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperSlice image={swiper2} text={"Title 2"} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperSlice image={swiper3} text={"Title 3"} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperSlice image={swiper4} text={"Title 4"} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperSlice image={swiper5} text={"Title 5"} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperSlice image={swiper6} text={"Title 6"} />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+export default Hero;
+
+const SwiperSlice = ({ image, text }) => {
+  return (
+    <div className="relative">
+      <img src={image} alt="swiper1" className="w-full h-[90vh] object-cover" />
+      {/* Light overlay on text */}
+      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40">
+        <div className="p-4 rounded-md">
+          <h2 className="text-white text-4xl font-bold">{text}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
